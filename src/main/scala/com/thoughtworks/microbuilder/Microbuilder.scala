e046d19a3040bd95e91fc16761048587717a07bb
sunchen115 <chen.sun@rea-group.com>
2015-11-18 15:40:21 +0800

    move setting from sample build.sbt to sbt plugin. :pear: Xiang

--- src/main/scala/com/thoughtworks/microbuilder/Microbuilder.scala
+++ src/main/scala/com/thoughtworks/microbuilder/Microbuilder.scala
@@ -1,7 +1,30 @@
 package com.thoughtworks.microbuilder
 
+import com.thoughtworks.microbuilder.sbtHaxe.BaseHaxePlugin.autoImport._
+import com.thoughtworks.microbuilder.sbtHaxe.{DependencyVersion, HaxeJavaPlugin}
 import sbt.AutoPlugin
+import sbt._
+import sbt.Keys._
 
 object Microbuilder extends AutoPlugin {
+  override def requires = HaxeJavaPlugin
 
+  val haxelibs = Map(
+    "continuation" -> DependencyVersion.SpecificVersion("1.3.2")
+  )
+
+
+  override lazy val projectSettings: Seq[Setting[_]] = (for (c <- AllTargetConfigurations ++ AllTestTargetConfigurations) yield {
+    haxeOptions in c ++= haxelibOptions(haxelibs)
+  }) ++
+    (for (c <- AllTargetConfigurations ++ AllTestTargetConfigurations) yield {
+    haxeOptions in c ++= Seq("-dce", "no")
+  }) ++
+    (for (c <- Seq(Compile, Test)) yield {
+    haxeOptions in c ++= Seq("-D", "scala")
+  }) :+
+    (libraryDependencies ++= Seq("com.thoughtworks.microbuilder" %% "json-stream" % "2.0.0" % HaxeJava classifier HaxeJava.name,
+      "com.thoughtworks.microbuilder" %% "json-stream" % "2.0.0",
+      "com.qifun" %% "haxe-scala-stm" % "0.1.4" % HaxeJava classifier HaxeJava.name
+    ))
 }
