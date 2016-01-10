3f443088b501967a34e0621d2f897e138659a3f5
Yang Bo <atryyang@thoughtworks.com>
2016-01-10 21:11:40 +0800

    JS SDK plugin should generate NPM package

--- src/main/scala/com/thoughtworks/microbuilder/MicrobuilderJsSdk.scala
+++ src/main/scala/com/thoughtworks/microbuilder/MicrobuilderJsSdk.scala
@@ -2,12 +2,13 @@ package com.thoughtworks.microbuilder
 
 import com.thoughtworks.microbuilder.sbtHaxe.BaseHaxePlugin.autoImport._
 import com.thoughtworks.microbuilder.sbtHaxe.HaxeJsPlugin
+import com.thoughtworks.microbuilder.sbtHaxe.HaxeJsNpmPlugin
 import sbt.Keys._
 import sbt.{AutoPlugin, _}
 
 object MicrobuilderJsSdk extends AutoPlugin {
 
-  override def requires = HaxeJsPlugin && MicrobuilderCommon
+  override def requires = HaxeJsPlugin && MicrobuilderCommon && HaxeJsNpmPlugin
 
   override lazy val projectSettings: Seq[Setting[_]] = {
     super.projectSettings ++ Seq(
